d80e41efa3ffde690ca314c6ba914f6c14e31497
ChristopherMendozaL <christophermendoza271@gmail.com>
2022-08-11 10:11:31 -0400

    FCM added

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:test/screens/home.dart';
 import 'package:test/services/notificationservice.dart';
 import 'package:test/util/theme_provider.dart';
 import 'package:firebase_core/firebase_core.dart';
+
 import 'package:firebase_messaging/firebase_messaging.dart';
 
 void main() async {
